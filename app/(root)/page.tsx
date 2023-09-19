1ec4b44571d610b8cb4a0ac7fa279fcc0d810805
Bojan <bojan-stanimirovski097@hotmail.com>
2023-09-19 06:37:28 +0200

    clerk auth setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
 
-
-export default function Home() {
+const SetupPage=() => {
     return (
-<div>
-  <p>hello admin dashboard</p>    
-  <Button size="default" variant="default" >Button</Button>
+<div className="p-4">
+<UserButton afterSignOutUrl="/"/>
 </div>
     )
   }
+
+  export default SetupPage;
