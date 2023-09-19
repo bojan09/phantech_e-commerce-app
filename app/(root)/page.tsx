56e01acb99c303edb568b73d11f0dbf6a7be6167
Bojan <bojan-stanimirovski097@hotmail.com>
2023-09-19 06:17:20 +0200

    project setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+    return (
+<div>
+  <p>hello admin dashboard</p>    
+  <Button size="default" variant="default" >Button</Button>
+</div>
+    )
+  }
