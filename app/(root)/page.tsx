6de2e267bd06b481a04e59a4c11c4dfe6ca0f511
Bojan <bojan-stanimirovski097@hotmail.com>
2023-09-22 06:49:01 +0200

    adding Modal,Providers,and Store

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,24 @@
-"use client"
+"use client";
 
-import { Modal } from "@/components/ui/modal";
+import { useEffect } from "react";
 
-const SetupPage=() => {
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetupPage = () => {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen();
+        }
+    }, [isOpen, onOpen]);
+
     return (
-<div className="p-4">
-    <Modal title="Test" description="test desc" isOpen onClose={()=>{}}>
-Children
-    </Modal>
-</div>
-    )
-  }
+        <div className="p-4">
+            Root page
+        </div>
+    );
+};
 
-  export default SetupPage;
+export default SetupPage;
