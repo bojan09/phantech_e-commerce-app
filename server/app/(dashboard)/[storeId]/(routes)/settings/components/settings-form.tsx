22271e0c6f53bd06e91b8f4920351bcd8fb8487b
Bojan <bojan-stanimirovski097@hotmail.com>
2023-09-30 08:07:29 +0200

    adding Billboard, settings,client,page,adding it in MainNav

--- server/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ server/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,18 +1,17 @@
 "use client";
 
 import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Store } from "@prisma/client";
-import { Trash } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { useState } from "react";
-import toast from "react-hot-toast";
 import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { Trash } from "lucide-react";
+import { Store } from "@prisma/client";
 import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
 
-import { Heading } from "@/components/ui/heading";
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import {
   Form,
   FormControl,
@@ -21,20 +20,21 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Heading } from "@/components/ui/heading";
 import { AlertModal } from "@/components/modals/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
 import { useOrigin } from "@/hooks/use-origin";
 
-interface SettingsFormProps {
-  initialData: Store;
-}
-
 const formSchema = z.object({
-  name: z.string().min(1),
+  name: z.string().min(2),
 });
 
 type SettingsFormValues = z.infer<typeof formSchema>;
+
+interface SettingsFormProps {
+  initialData: Store;
+}
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   const params = useParams();
@@ -54,9 +54,9 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
       setLoading(true);
       await axios.patch(`/api/stores/${params.storeId}`, data);
       router.refresh();
-      toast.success("Store updated");
-    } catch (error) {
-      toast.error("Something went wrong");
+      toast.success("Store updated.");
+    } catch (error: any) {
+      toast.error("Something went wrong.");
     } finally {
       setLoading(false);
     }
@@ -64,12 +64,13 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
 
   const onDelete = async () => {
     try {
+      setLoading(true);
       await axios.delete(`/api/stores/${params.storeId}`);
       router.refresh();
       router.push("/");
-      toast.success("Store deleted");
-    } catch (error) {
-      toast.error("Make sure you removed all categories and products first");
+      toast.success("Store deleted.");
+    } catch (error: any) {
+      toast.error("Make sure you removed all products and categories first.");
     } finally {
       setLoading(false);
       setOpen(false);
@@ -85,9 +86,16 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
         loading={loading}
       />
       <div className="flex items-center justify-between">
-        <Heading title="Settings" description="Manage store preferences" />
-
-        <Button variant="destructive" size="icon" onClick={() => setOpen(true)}>
+        <Heading
+          title="Store settings"
+          description="Manage store preferences"
+        />
+        <Button
+          disabled={loading}
+          variant="destructive"
+          size="sm"
+          onClick={() => setOpen(true)}
+        >
           <Trash className="h-4 w-4" />
         </Button>
       </div>
@@ -120,13 +128,13 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
             Save changes
           </Button>
         </form>
-        <Separator />
-        <ApiAlert
-          title="NEXT_PUBLIC_API_URL"
-          description={`${origin}/api/${params.storeId}`}
-          variant="public"
-        ></ApiAlert>
       </Form>
+      <Separator />
+      <ApiAlert
+        title="NEXT_PUBLIC_API_URL"
+        variant="public"
+        description={`${origin}/api/${params.storeId}`}
+      />
     </>
   );
 };
