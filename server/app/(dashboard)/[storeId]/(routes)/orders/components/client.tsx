ac203c0d67054f7167010f07cec3d05aedc4a2fe
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-08 10:42:15 +0200

    Orders: Client and Columns

--- server/app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ server/app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -1,42 +1,23 @@
 "use client";
 
-import { useParams, useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
-import { ApiList } from "@/components/ui/api-list";
-import { BillboardColumn, columns } from "./columns";
+import { OrderColumn, columns } from "./columns";
 
-interface BillboardClientProps {
-  data: BillboardColumn[];
+interface OrderClientProps {
+  data: OrderColumn[];
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
-  const router = useRouter();
-  const params = useParams();
-
+export const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
   return (
     <>
-      <div className="flex items-center justify-between">
-        <Heading
-          title={`Billboards (${data.length})`}
-          description="Manage billboards for your store"
-        />
-        <Button
-          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
-        >
-          <Plus className="mr-2 h-4 w-4" />
-          Add new
-        </Button>
-      </div>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
       <Separator />
-      <DataTable columns={columns} data={data} searchKey="label" />
-      <Heading title="API" description="API calls for Billboards" />
-      <Separator />
-      <ApiList entityName="billboards" entityIdName="billboardId" />
+      <DataTable columns={columns} data={data} searchKey="products" />
     </>
   );
 };
