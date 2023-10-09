1c0adf07f8d8f33b3235164b62b76321c7861786
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-09 21:11:20 +0200

    navbar-actions - minor bug fix

--- client/components/navbar-actions.tsx
+++ client/components/navbar-actions.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import Button from "@/components/ui/button";
 import { ShoppingBag } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+
+import Button from "@/components/ui/button";
 
 const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -22,7 +23,7 @@ const NavbarActions = () => {
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex items-center rounded-full bg-black px-4 py-2">
         <ShoppingBag size={20} color="white" />
-        <span className="ml-2 text-sm font-medium text-white">0</span>
+        <span className="ml-2 text-sm font-medium text-white">0 </span>
       </Button>
     </div>
   );
