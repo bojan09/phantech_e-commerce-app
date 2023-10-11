5f27ef29d360b9705209dab52341c2b233c27e62
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-11 14:43:32 +0200

    Products: added ProductList, ProductCard, IconButton, Currency,

--- client/app/(routes)/page.tsx
+++ client/app/(routes)/page.tsx
@@ -13,11 +13,11 @@ const HomePage = async () => {
 
   return (
     <Container>
-      <div className="space-y-10 pb-10 ">
+      <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
-      </div>
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={[]} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   );
