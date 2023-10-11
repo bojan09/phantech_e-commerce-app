c81ea358fbee74c583efb5697274d4ebd700d09f
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-11 14:07:46 +0200

    Product: added no-results page

--- client/app/(routes)/page.tsx
+++ client/app/(routes)/page.tsx
@@ -17,7 +17,7 @@ const HomePage = async () => {
         <Billboard data={billboard} />
       </div>
       <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={products} />
+        <ProductList title="Featured Products" items={[]} />
       </div>
     </Container>
   );
