71349b8e63ec4e4cf41ac0f45c81bda8b2caf44b
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-13 20:38:39 +0200

    added Gallery Tab, and Gallery

--- client/components/gallery/index.tsx
+++ client/components/gallery/index.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import NextImage from "next/image";
 import { Tab } from "@headlessui/react";
-import Image from "next/image";
-import { Image as ImageType } from "@/types";
+
+import { Image } from "@/types";
+
+import GalleryTab from "./gallery-tab";
 
 interface GalleryProps {
-  images: ImageType[];
+  images: Image[];
 }
 
-const Gallery: React.FC<GalleryProps> = ({ images }) => {
+const Gallery: React.FC<GalleryProps> = ({ images = [] }) => {
   return (
     <Tab.Group as="div" className="flex flex-col-reverse">
       <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
@@ -18,6 +21,20 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
           ))}
         </Tab.List>
       </div>
+      <Tab.Panels className="aspect-square w-full">
+        {images.map((image) => (
+          <Tab.Panel key={image.id}>
+            <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
+              <NextImage
+                fill
+                src={image.url}
+                alt="Image"
+                className="object-cover object-center"
+              />
+            </div>
+          </Tab.Panel>
+        ))}
+      </Tab.Panels>
     </Tab.Group>
   );
 };
