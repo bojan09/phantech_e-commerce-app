8e543354c67192a611053017bec90eaeb61b1a95
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-15 15:01:44 +0200

    added Checkout to Summary

--- client/app/(routes)/cart/components/summary.tsx
+++ client/app/(routes)/cart/components/summary.tsx
@@ -16,6 +16,15 @@ const Summary = () => {
     return total + Number(item.price);
   }, 0);
 
+  const onCheckout = async () => {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
+
+      { productIds: items.map((item) => item.id) }
+    );
+    window.location = response.data.url;
+  };
+
   return (
     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
       <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
@@ -27,7 +36,9 @@ const Summary = () => {
           <Currency value={totalPrice} />
         </div>
       </div>
-      <Button className="w-full mt-6">Checkout</Button>
+      <Button onClick={onCheckout} className="w-full mt-6">
+        Checkout
+      </Button>
     </div>
   );
 };
